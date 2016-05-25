b2d2e4ae70c06785f0f88391e78cdfa43e9200e0
Zhihao Yuan <lichray@gmail.com>
2016-05-24 20:25:42 -0500

    Add code to demonstrate normal distribution.

--- example.cc
+++ example.cc
@@ -1,5 +1,23 @@
 #include "randutils.hpp"
+
 #include <iostream>
+#include <cmath>
+
+template <typename Iter>
+auto stddev(Iter first, Iter last)
+{
+    using T = typename std::iterator_traits<Iter>::value_type;
+
+    auto sum = std::accumulate(first, last, T());
+    auto n = std::distance(first, last);
+    auto m = sum / n;
+    auto accum = std::accumulate(first, last, T(), [=](T v, T d)
+                                 {
+                                     return v + (d - m) * (d - m);
+                                 });
+
+    return std::sqrt(accum / n);
+}
 
 int main()
 {
@@ -11,4 +29,12 @@ int main()
 
     rng.use<std::mt19937>();
     std::cout << rng.uniform(1000, 2000) << '\n';
+
+    std::vector<double> v(10000);
+    rng.generate<std::normal_distribution>(begin(v), end(v));
+    std::cout << stddev(begin(v), end(v)) << '\n';
+
+    for (auto& x : v)
+        x = rng.variate<double>();
+    std::cout << stddev(begin(v), end(v)) << '\n';
 }
